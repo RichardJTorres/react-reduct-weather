62444f02a59562ca54aee1ecace6bb77b1544752
Rich Torres <richjtorres@gmail.com>
2016-07-19 00:02:21 -0700

    fixing spelling mistake

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,7 +4,7 @@ import {API_KEY} from '../settings';
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 export const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
 
-export function featchWeather(city) {
+export function fetchWeather(city) {
   const url = `${ROOT_URL}&q=${city},us`;
   const request = axios.get(url);
 
